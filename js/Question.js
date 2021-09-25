38964362393121dc250ff2398fae8272ccf7e7d3
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2021-09-25 23:19:55 +0530

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -24,15 +24,15 @@ class Question {
     this.title.position(350, 0);
 
     this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    this.question.position(150, 80);
+    
     this.option1.html("1: Everyone " );
-    this.option1.position(150, 100);
+  
     this.option2.html("2: Envelope" );
-    this.option2.position(150, 120);
+
     this.option3.html("3: Estimate " );
-    this.option3.position(150, 140);
+
     this.option4.html("4: Example" );
-    this.option4.position(150, 160);
+ 
 
     this.input1.position(150, 230);
     this.input2.position(350, 230);
