720c67403d42df6706d2c668dff576e3509d6793
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2021-10-21 05:41:27 +0530

    Update Quiz.js

--- js/Quiz.js
+++ js/Quiz.js
@@ -1,19 +1,6 @@
 class Quiz {
   constructor(){}
 
-  getState(){
-    var gameStateRef  = database.ref('gameState');
-    gameStateRef.on("value",function(data){
-       gameState = data.val();
-    })
-
-  }
-
-  update(state){
-    database.ref('/').update({
-      gameState: state
-    });
-  }
 
   async start(){
     if(gameState === 0){
